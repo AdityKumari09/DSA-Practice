943b6a6c6c65c8bdcf2a38ce3045332a91df05da
AdityKumari09 <kumariadity09@gmail.com>
2026-02-28 22:51:19 +0530

    committed

---
+++ 003/Test.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the digit :- ");
+        int n = sc.nextInt();
+        int temp = n;
+        int rev = 0;
+
+        while (n != 0)
+        {
+            rev = rev * 10 + n % 10;
+            n = n / 10;
+        }
+
+        if (temp == rev)
+        {
+            System.out.print("The reverse of " + temp + " is " + rev + " and therefor it is palindrome");
+        }
+        else {
+            System.out.print("The reverse of " + temp + " is " + rev + " and therefor it is not palindrome");   
+        }
+
+    }
+}
+
+/*
+    Problem Statement: Given an integer N, return true if it is a palindrome 
+    else return false.
+
+    Example 1:
+    Input:N = 4554
+    Output:Palindrome Number
+    Explanation: The reverse of 4554 is 4554 and therefore it is palindrome number
+                                            
+    Example 2:
+    Input:N = 7789          
+    Output: Not Palindrome
+    Explanation: The reverse of number 7789 is 9877 and therefore it is not palindrome
+            
+ */
+
