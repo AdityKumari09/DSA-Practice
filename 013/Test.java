acf1b217cc4302b0641e726e6f5deaad420ade18
AdityKumari09 <kumariadity09@gmail.com>
2026-03-06 21:55:42 +0530

    committed

--- 013/Test.java
+++ 013/Test.java
@@ -7,6 +7,27 @@ public class Test {
         System.out.print("Enter the number :- ");
         int n = sc.nextInt();
 
+        for (int i = 0; i < n; i++)
+        {
+            int count = 1;
+
+            for (int j = 1; j <= n - i; j++)
+            {
+                System.out.print(count);
+                count++;
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
         
     }
 }
+/*
+    12345
+    1234
+    123
+    12
+    1
+*/
