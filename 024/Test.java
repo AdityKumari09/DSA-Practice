025d78091d7680bd59555c70788735ecf0196466
AdityKumari09 <kumariadity09@gmail.com>
2026-03-10 09:54:07 +0530

    committed

---
+++ 024/Test.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Test {
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows :- ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++)
+        {
+            char ch = 'A';
+
+            for (int j = 1; j <= n - i; j++)
+            {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= i; j++)
+            {
+                System.out.print(ch);
+                ch++;
+            }
+
+            ch -= 2;
+
+            for (int j = 1; j < i; j++)
+            {
+                System.out.print(ch);
+                ch--;
+            }
+
+            for (int j = 1; j <= n - i; j++)
+            {
+                System.out.print(" ");
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
+    }   
+}
+
+/*
+    
+       A
+      ABA
+     ABCBA
+    ABCDCBA
+*/
