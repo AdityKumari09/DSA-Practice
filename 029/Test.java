31a6602d1cc9b841b2d1ecd16c45ac706e1e84a3
AdityKumari09 <kumariadity09@gmail.com>
2026-03-21 08:46:57 +0530

    committed

---
+++ 029/Test.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Test{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number required :- ");
+        int n = sc.nextInt();
+
+        int size = 2 * n - 1;
+
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                int top = i;
+                int left = j;
+                int bottom = size - 1 - i;
+                int right = size - 1 - j;
+
+                int min = Math.min(Math.min(top, bottom), Math.min(right, left));
+
+                int value = n - min;
+
+                System.out.print(value + " ");
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
+
+/*
+    4 4 4 4 4 4 4
+    4 3 3 3 3 3 4
+    4 3 2 2 2 3 4
+    4 3 2 1 2 3 4
+    4 3 2 2 2 3 4
+    4 3 3 3 3 3 4
+    4 4 4 4 4 4 4
+*/
