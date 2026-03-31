7947df01eefa62bc5e2a4f6e5646e5b25dd7608a
AdityKumari09 <kumariadity09@gmail.com>
2026-03-31 13:01:47 +0530

    committed

---
+++ 045_Bubble_Sort/Test.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of array :- ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            boolean swapped = false;
+            // for each step max item will come at the last respective index
+            for (int j = 1; j < n - i; j++)
+            {
+                if (arr[j] < arr[j - 1])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            /* 
+                if no swapping is done for a particular value of i, it means arr
+                is already sorted, then stop the loop
+             */
+            if (swapped == false)
+            {
+                break;
+            }
+        }
+
+        for (int nums : arr)
+        {
+            System.out.print(nums + " ");
+        }
+    }    
+}
