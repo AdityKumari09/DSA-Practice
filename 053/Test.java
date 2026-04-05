7e1298be83033eff04b9a56b24f84cf4ccd09f23
AdityKumari09 <kumariadity09@gmail.com>
2026-04-05 14:46:20 +0530

    committed

---
+++ 053/Test.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of array :- ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] > max)
+            {
+                max = arr[i];
+            }
+
+            if (arr[i] < min)
+            {
+                min = arr[i];
+            }
+
+        }
+
+        int second_largest = Integer.MIN_VALUE;
+        int second_smallest = Integer.MAX_VALUE;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] > second_largest && arr[i] < max)
+            {
+                second_largest = arr[i];
+            }
+
+            if (arr[i] < second_smallest && arr[i] > min)
+            {
+                second_smallest = arr[i];
+            }
+        }
+
+        if (second_largest == Integer.MIN_VALUE)
+        {
+            second_largest = -1;
+        }
+
+        if (second_smallest == Integer.MAX_VALUE)
+        {
+            second_smallest = -1;
+        }
+
+        System.out.print(second_largest + " " + second_smallest);
+    }
+}
+
+/*
+    Problem: Given an array, find the second smallest and second largest element
+    in the array. Print ‘-1’ in the event that either of them doesn’t exist.
+
+    Input:
+    [1, 2, 4, 7, 7, 5]  
+    Output:
+    Second Smallest : 2  
+    Second Largest : 5  
+*/
