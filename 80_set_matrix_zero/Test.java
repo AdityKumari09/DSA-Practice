f03146c4685a0e6a38b0a2f77520dbd08687169f
AdityKumari09 <kumariadity09@gmail.com>
2026-04-29 23:06:58 +0530

    committed

--- 80_set_matrix_zero/Test.java
+++ 80_set_matrix_zero/Test.java
@@ -3,6 +3,68 @@ import java.util.Scanner;
 public class Test {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows :- ");
+        int m = sc.nextInt();
+
+        System.out.print("Enter the number of column :- ");
+        int n = sc.nextInt();
+
+        int[][] matrix = new int[m][n];
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("Matrix after setting zero :- ");
+        setZeros(matrix);
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                System.out.print(matrix[i][j] + " ");
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
+
+    public static void setZeros(int[][] matrix)
+    {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        boolean[] row = new boolean[m];
+        boolean[] column = new boolean[n];
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (matrix[i][j] == 0)
+                {
+                    row[i] = true;
+                    column[j] = true;
+                }
+            }
+        }
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (row[i] || column[j])
+                {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
     }
 }
 /*
